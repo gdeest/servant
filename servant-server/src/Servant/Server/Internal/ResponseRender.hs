31fc02936b8dca362ce91faaa42ffa8eecaa4967
Gaël Deest <gael.deest@gmail.com>
2025-12-11 15:29:45 +0100

    Make Verb and NoContentVerb delegate to MultiVerb
    This is the first step toward unifying endpoint types around MultiVerb.
    
    Changes:
    - Verb's HasServer instances now delegate to MultiVerb instead of using
      methodRouter directly
    - NoContentVerb delegates to MultiVerb with RespondAs '() 204
    - Verb and NoContentVerb method parameter changed from polymorphic (k1)
      to strict (StdMethod) for consistency with MultiVerb
    - Stream's method parameter also changed to StdMethod for consistency
    - Added KnownStatus constraint to Verb instances (was KnownNat)
    - Added ResponseRender instance for Respond with Headers to support
      the delegation
    
    Removed dead code:
    - methodRouter (was only used by Verb)
    - noContentRouter (was only used by NoContentVerb)
    - responseLBS import (no longer needed)
    
    Test changes:
    - Added KnownStatus instances for non-standard test status codes
      (210, 214, 280)

--- servant-server/src/Servant/Server/Internal/ResponseRender.hs
+++ servant-server/src/Servant/Server/Internal/ResponseRender.hs
@@ -25,6 +25,7 @@ import Servant.API.ContentTypes
   , mimeRender
   )
 import Servant.API.MultiVerb
+import Servant.API.ResponseHeaders (GetHeaders (..), Headers, getResponse)
 import Servant.API.Status
 import Servant.API.Stream (FramingRender (..), SourceIO)
 import Servant.API.UVerb.Union
@@ -172,6 +173,7 @@ instance
         }
 
 instance
+  {-# OVERLAPPABLE #-}
   (AllMimeRender cs a, KnownStatus s)
   => ResponseRender cs (Respond s desc a)
   where
@@ -192,6 +194,29 @@ instance
             { statusCode = statusVal (Proxy @s)
             , responseBody = body
             , headers = mempty
+            }
+
+-- | Instance for Respond with Headers - extracts headers from the Headers wrapper
+-- This enables Verb with Headers to delegate to MultiVerb
+instance
+  {-# OVERLAPPING #-}
+  (AllMimeRender cs a, GetHeaders (Headers h a), KnownStatus s)
+  => ResponseRender cs (Respond s desc (Headers h a))
+  where
+  type ResponseStatus (Respond s desc (Headers h a)) = s
+  type ResponseBody (Respond s desc (Headers h a)) = BSL.ByteString
+
+  responseRender (AcceptHeader acc) headersVal =
+    M.mapAcceptMedia (map (uncurry mkRenderOutput) (allMimeRender (Proxy @cs) (getResponse headersVal))) acc
+    where
+      responseHeaders = Seq.fromList (getHeaders headersVal)
+      mkRenderOutput :: M.MediaType -> BSL.ByteString -> (M.MediaType, InternalResponse BSL.ByteString)
+      mkRenderOutput c body =
+        (c,) . addContentType' c $
+          InternalResponse
+            { statusCode = statusVal (Proxy @s)
+            , responseBody = body
+            , headers = responseHeaders
             }
 
 addContentType :: forall ct a. Accept ct => InternalResponse a -> InternalResponse a
--- servant/src/Servant/API/Stream.hs
+++ servant/src/Servant/API/Stream.hs
@@ -59,7 +59,7 @@ import Servant.Types.SourceT
 -- | A Stream endpoint for a given method emits a stream of encoded values at a
 -- given @Content-Type@, delimited by a @framing@ strategy.
 -- Type synonyms are provided for standard methods.
-data Stream (method :: k1) (status :: Nat) (framing :: Type) (contentType :: Type) (a :: Type)
+data Stream (method :: StdMethod) (status :: Nat) (framing :: Type) (contentType :: Type) (a :: Type)
   deriving (Generic, Typeable)
 
 type StreamGet = Stream 'GET 200
